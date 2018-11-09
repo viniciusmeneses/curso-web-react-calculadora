cab1349b76faea796d8082c59cb0a93c350d25f4
vini23hot <vini23hot@gmail.com>
2018-11-09 14:51:55 -0200

    Added comments to Calculator component

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,42 +4,50 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+// Valores iniciais do estado da calculadora
 const initialState = {
+  // Valor que aparecerá no display da calculadora
   displayValue: '0',
+  // Se for true, no próximo digito (chamada do método addDigit()), o display será limpado
   clearDisplay: false,
+  // Define a operação atual (+, -, *, /, =)
   operation: null,
+  // Array de valores digitados na calculadora, que irão ser usados junto com a operação para fazer o calculo
   values: [0, 0],
+  // Índice da posição atual do array onde o número do display será digitado
   current: 0
 }
 
 export default class Calculator extends Component {
 
+  // Define o estado inicial
   state = { ...initialState }
 
   clearMemory = () => {
+    // Reseta a calculadora passando o estado inicial
     this.setState({ ...initialState })
   }
 
   setOperation = (operation) => {
+    // Se estiver digitando o primeiro número
+    // passa a operação recebida, troca para o segundo número
+    // e seta true para limpar o display para o próximo numero
     if (this.state.current === 0) {
       this.setState({
         operation,
         current: 1,
         clearDisplay: true
       })
+    // Se já estiver no segundo numero
     } else {
       const equals = operation === '='
       const currentOperation = this.state.operation
 
+      // Copia os valores do array do state para a constante values
       const values = [...this.state.values]
 
-      // try {
-      //   values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
-      // }
-      // catch(e) {
-      //   values[0] = this.state.values[0]
-      // }
-
+      // Se a operação atual no estado for...
+      // Realiza o calculo, salva na posição 0 do array e zera a posição 1
       switch (currentOperation) {
         case '+':
           values[0] = values[0] + values[1]
@@ -53,12 +61,18 @@ export default class Calculator extends Component {
         case '/':
           values[0] = values[0] / values[1]
           break
+        // Se a operação for '=' então mantém o primeiro numero do array
         default:
           values[0] = this.state.values[0]
       }
 
       values[1] = 0
 
+      // Seta o estado passando o valor do indice 0 do array para o display
+      // Se a operação for '=', seta como null
+      // Se a operação for '=', seta como atual o primeiro indice
+      // Limpa o display se a operação não for igual
+      // Passa os valores do array
       this.setState({
         displayValue: values[0],
         operation: equals ? null : operation,
@@ -70,23 +84,31 @@ export default class Calculator extends Component {
   }
 
   addDigit = (n) => {
+    // Se já existir um ponto no display, não inclui outro
     if (n === '.' && this.state.displayValue.includes('.')) return
 
+    // Se o display for somente 0 ou estiver setado true no estado, limpa o display
     const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    // Se o display precisar ser limpo...
     const currentValue = clearDisplay ? '' : this.state.displayValue
+    // Adiciona o numero digitado ao valor atual
     const displayValue = currentValue + n
 
+    // Seta o diplay e desabilita a opção de limpar o display no próximo digito
     this.setState({
       displayValue,
       clearDisplay: false
     })
 
+    // Se for um dígito/não for '.'
     if (n !== '.') {
+      // Pega o valor do display e altera o valor no indice atual dos valores (array)
       const i = this.state.current
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[i] = newValue
 
+      // Seta no estado
       this.setState({
         values
       })
