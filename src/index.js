8baa29e4eb8b883fcf335f0b52182edf31d1b3bc
vini23hot <vini23hot@gmail.com>
2018-11-09 15:16:23 -0200

    Improved styles

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import Calculator from './main/Calculator';
 
 ReactDOM.render((
   <div>
-    <h1>Calculadora</h1>
+    <h1>Calculator</h1>
     <Calculator />
   </div>), document.getElementById('root'));
